53b3c4b116e5a930b931ad8c21ee56fff69ac93c
PTM <peter-mai@hotmail.com>
2015-11-25 13:09:42 +0100

    Handin2 created
    New folder created with empty MVC

---
+++ Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HandIn2_FoodProcessor.MvcApplication" Language="C#" %>
---
+++ Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HandIn2_FoodProcessor.Startup))]
+namespace HandIn2_FoodProcessor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
