8e6c2effd59fb85a31eb53e9da2f5777195f0884
201205925 <201205925@post.au.dk>
2015-12-08 17:10:47 +0100

    fuk

--- Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Controllers/HomeController.cs
+++ Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using HandIn2_FoodProcessor.Models;
 
 
 
@@ -11,10 +13,13 @@ namespace HandIn2_FoodProcessor.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            var userID = User.Identity.GetUserName();
+            List<Posts> potato = db.Posts.Where(c => c.User.Name == userID).ToList();
 
-            return View();
+            return View(potato);
         }
     }
 }
