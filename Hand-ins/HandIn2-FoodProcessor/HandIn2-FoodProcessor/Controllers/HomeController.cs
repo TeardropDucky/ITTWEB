dc2a253018e0482cbc25be2b59a821536e0bf095
201205925 <201205925@post.au.dk>
2015-12-09 17:12:44 +0100

    FML

--- Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Controllers/HomeController.cs
+++ Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Controllers/HomeController.cs
@@ -13,9 +13,11 @@ namespace HandIn2_FoodProcessor.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        Random rnd = new Random();
         private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            
             var userID = User.Identity.GetUserName();
             db.Configuration.LazyLoadingEnabled = true;
             if (db.UserInfoes.Where(c => c.Name == userID).ToList().Count == 0)
@@ -26,17 +28,25 @@ namespace HandIn2_FoodProcessor.Controllers
                 db.UserInfoes.Add(user);
                 db.SaveChanges();
 
-                var derp = new Posts();
-                derp.User = user;
-                derp.PostDate = DateTime.Now;
-                derp.Consumable = db.Consumables.Find(1);
-                derp.GramProtein = 100;
-                db.Posts.Add(derp);
-                db.SaveChanges();
+                for (int i = 0; i < 10; i++)
+                {
+                    var derp = new Posts();
+                    derp.User = user;
+                    derp.PostDate = DateTime.Now;
+                    derp.Consumable = db.Consumables.Find(1);
+                    derp.GramProtein = rnd.Next(1, 99);
+                    db.Posts.Add(derp);
+                    db.SaveChanges();
+                }
             }
             
             
             List<Posts> potato = db.Posts.Where(c => c.User.Name == userID).ToList();
+            
+            foreach (var i in potato)
+            {
+                i.Consumable = db.Consumables.Find(rnd.Next(1,9));
+            }
            
             
             return View(potato);
--- Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Scripts/Angular/proteinneed-controller.js
+++ Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Scripts/Angular/proteinneed-controller.js
@@ -1,16 +1,33 @@
 ﻿angular.module('proteinNeed', [])
     .controller('pNeedController', function ($scope, $http) {
+        $scope.Math = window.Math;
 
         $scope.product = {
-            potato: ''
+            picked: ''
         };
         $scope.showProduct = function () {
             return $scope.product.potato;
         };
+
         $scope.consumed = 0;
-        $scope.weight = 1;
-        $scope.Math = window.Math;
-        $scope.pNeed = function () {
+        $scope.pEaten = function () {
             return $scope.weight + $scope.consumed;
+        };
+
+        $scope.weight = 0;
+        //hurra for copy pasta
+        $scope.addresses = [
+        { 'state': 'Normal' },
+        { 'state': 'Svækket' },
+        { 'state': 'Trænning' }
+        ];
+        $scope.lov_state = [
+            { 'lookupCode': 0.8, 'description': 'Normal' },
+            { 'lookupCode': 1.5, 'description': 'Svækket' },
+            { 'lookupCode': 2.0, 'description': 'Trænning' }
+        ];
+
+        $scope.pNeed = function(){
+            return $scope.weight + $scope.addresses.state;
         };
     });
