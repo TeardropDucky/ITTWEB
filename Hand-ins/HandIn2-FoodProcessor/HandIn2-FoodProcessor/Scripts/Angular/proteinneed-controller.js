fda99cde5f3f55c8d9c2e8c189d74c4e50642fe2
201205925 <201205925@post.au.dk>
2015-12-09 16:06:01 +0100

    Revert "Html updates"
    This reverts commit 0679b096a3874e6343d9676d14da11b21a28150c.

--- Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Scripts/Angular/proteinneed-controller.js
+++ Hand-ins/HandIn2-FoodProcessor/HandIn2-FoodProcessor/Scripts/Angular/proteinneed-controller.js
@@ -8,11 +8,7 @@
             return $scope.product.potato;
         };
         $scope.consumed = 0;
-        $scope.weight = 0;
-        $scope.psycalState = 0.8;
-        $scope.showProteinNeed = function () {
-            return $scope.weight * $scope.psycalState;
-        };
+        $scope.weight = 1;
         $scope.Math = window.Math;
         $scope.pNeed = function () {
             return $scope.weight + $scope.consumed;
